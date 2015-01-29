20fc545105f8cbcbd8b706d0699f498e97f71823
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2015-01-29 15:24:00 +0100

    Merged the two equal geometry frag shaders

--- Source/User/Rendering/Programs.cpp
+++ Source/User/Rendering/Programs.cpp
@@ -57,10 +57,14 @@ namespace Rendering {
     return program;
   }
 
-  static Backend::ProgramHandle initializeGlobalBlockProgram(const char *name) {
-    Backend::ProgramHandle program = initializeStandardProgram(name);
+  static void attachGlobalBlock(Backend::ProgramHandle program) {
     Backend::UniformBlockHandle block = Backend::getUniformBlock(program, "global");
     Backend::setUniformBlockIndex(program, block, 0);
+  }
+
+  static Backend::ProgramHandle initializeGlobalBlockProgram(const char *name) {
+    Backend::ProgramHandle program = initializeStandardProgram(name);
+    attachGlobalBlock(program);
     return program;
   }
 
@@ -81,12 +85,33 @@ namespace Rendering {
     return program;
   }
 
+  static Backend::ProgramHandle initializeGeometryProgram(const char *type) {
+    Backend::ProgramHandle program = Backend::createProgram();
+
+    char vertexPathBuffer[128] = "Geometry";
+    strcat(vertexPathBuffer, type);
+    strcat(vertexPathBuffer, ".vert");
+    loadSource(vertexPathBuffer);
+    Backend::ShaderHandle vertexShader = Backend::createShader(Backend::ShaderType::Vertex, sourceBuffer);
+    Backend::attachShader(program, vertexShader);
+
+    loadSource("Geometry.frag");
+    Backend::ShaderHandle fragmentShader = Backend::createShader(Backend::ShaderType::Fragment, sourceBuffer);
+    Backend::attachShader(program, fragmentShader);
+
+    Backend::linkProgram(program);
+
+    attachGlobalBlock(program);
+
+    return program;
+  }
+
   namespace Programs {
     HandleList handles;
 
     void initialize() {
-      handles.geometryBone = initializeGlobalBlockProgram("GeometryBone");
-      handles.geometryStatic = initializeGlobalBlockProgram("GeometryStatic");
+      handles.geometryBone = initializeGeometryProgram("Bone");
+      handles.geometryStatic = initializeGeometryProgram("Static");
       handles.merge = initializeStandardProgram("Merge");
       handles.pointLight = initializeGlobalBlockProgram("PointLight");
       handles.shadowBaseBone = initializeShadowBaseBoneProgram();
