ab3c65b66936f26ee7049c3a6d3abafe98eaf5aa
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2015-03-06 14:20:13 +0100

    Added skeleton for ServerGame

---
+++ ServerGame/ServerGame.h
@@ -0,0 +1,8 @@
+#ifndef SERVER_GAME_H
+#define SERVER_GAME_H
+
+class ServerGame {
+
+};
+
+#endif
--- UserGame/UserGame.cpp
+++ UserGame/UserGame.cpp
@@ -1,8 +1,10 @@
+#include "ServerGame/ServerGame.h"
 #include "UserGame/ClientGame.h"
 #include "UserGame/UserGame.h"
 
 namespace UserGame {
   ClientGame clientGame;
+  ServerGame *serverGame = nullptr;
 
   void initialize(uint16_t resolutionWidth, uint16_t resolutionHeight) {
     clientGame.initialize(resolutionWidth, resolutionHeight);
