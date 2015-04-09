721b6b009650fa4492873240976eeed08629d09b
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2015-04-09 16:36:44 +0200

    Fixie::TransformFactory

---
+++ Libraries/Fixie/Library/Include/Fixie/TransformFactory.h
@@ -0,0 +1,12 @@
+#ifndef FIXIE_TRANSFORM_FACTORY_H
+#define FIXIE_TRANSFORM_FACTORY_H
+
+#include "Fixie/Matrix4.h"
+
+namespace Fixie {
+  namespace TransformFactory {
+    Matrix4 translation(Vector3 translation);
+  }
+}
+
+#endif
