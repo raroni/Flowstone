18f851d84dc38107eb2a84c4ba312e2ea5341d35
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2015-05-10 18:29:33 +0200

    First half of new improved build setup

---
+++ Platforms/MacOSX/Make/ClientHeaderDirs.mk
@@ -0,0 +1,14 @@
+CLIENT_HEADER_DIRS =\
+  Libraries/Conrad/Include\
+  Libraries/Quanta/Include\
+  Libraries/Fixie/Library/Include\
+  Libraries/Piper/Include\
+  Sys/File/Include\
+  Sys/Net/Include\
+  Sys/Key/Include\
+  Sys/GFX/Include\
+  Sys/GFX/OpenGL/Include\
+  Sys/Time/Include\
+  Sys/Thread/Include\
+  $(PLATFORM_DIR)/Include\
+  .
---
+++ Platforms/MacOSX/Make/ClientShaders.mk
@@ -0,0 +1,15 @@
+CLIENT_SHADERS =\
+  Downsample.frag\
+  FullscreenQuad.vert\
+  Geometry.frag\
+  GeometryBone.vert\
+  GeometryStatic.vert\
+  Merge.frag\
+  PointLight.frag\
+  PointLight.vert\
+  SSAOBlur.frag\
+  SSAOGrain.frag\
+  ShadowBase.frag\
+  ShadowBaseBone.vert\
+  ShadowBaseStatic.vert\
+  ShadowBlur.frag
